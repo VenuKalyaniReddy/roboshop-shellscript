c93c632c7d3f9a4217474434484ee485f0508892
unknown <kalyaninalamaru.com>
2024-06-05 17:15:12 +0530

    roboshop shell scripts

---
+++ catalouge.sh
@@ -0,0 +1,46 @@
+#!bin/bash
+
+ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="\tmp\ $0-$TIMESTAMP.log"
+echo "script started exectuing at $TIMESTAMP" &>>$LOGFILE
+
+VALIDATE () {
+
+if [ $1- ne 0 ]
+
+then
+
+    echo -e "$2...$R FAILED $N"
+    exit 1
+else
+    echo -e "$2 ....$G SUCCESS $N"
+
+fi
+
+if [ $ID -ne 0 ]
+
+then
+
+    echo -e "$R ERROR :: Please run this script with root access $N"
+    exit 1
+else
+    echo "you are root user"
+fi
+
+}
+
+dnf module disable nodejs -y &>>$LOGFILE
+VALIDATE $? "Disabling current NODEJS"
+dnf module enable nodejs:18 -y &>>$LOGFILE
+VALIDATE $? "Enabling the nodejs:18"
+dnf install nodejs -y &>>$LOGFILE
+VALIDATE $? "Installing nodejs"
+
+
