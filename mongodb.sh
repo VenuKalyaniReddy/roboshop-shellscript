8d76bdfb540972535749f8b1d3033d7034093ab0
unknown <kalyaninalamaru.com>
2024-06-05 13:26:17 +0530

    roboshop shell scripts

--- mongodb.sh
+++ mongodb.sh
@@ -1,3 +1,55 @@
+# #!/bin/bash
+
+# ID=$(id -u)
+
+# R="\e[31m"
+# G="\e[32m"
+# Y="\e[33m"
+# N="\e[0m"
+
+# TIMESTAMP= $(date +%F-%H-%M-%S)
+# LOGFILE="/tmp/$0-$TIMESTAMP.log"
+#     echo -e "script started executing at $TIMESTAMP" &>>$LOGFILE
+
+# VALIDATE () {
+
+# if [ $1 -ne 0 ]
+# then 
+#     echo -e "$2......$R FAILED $N"
+#     exit 1
+# else
+#     echo -e "$2.....$G SUCCESS $N"
+
+# fi
+
+# }
+
+# if [ $ID -ne 0 ]
+
+# then 
+#     echo -e "$R ERROR::Please run this script with root access $N"
+#     exit 1
+# else
+#     echo "you are root user "
+# fi
+
+# cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
+
+# VALIDATE $? "Copied Mongodb repo"
+
+# yum install mongodb-org -y &>>$LOGFILE
+
+# VALIDATE $? "Installing Mongodb"
+
+# systemctl start mongod &>>$LOGFILE
+
+# VALIDATE $? "Enabling Mongodb"
+
+# systemctl start mongodb &>>$LOGFILE
+
+# VALIDATE $? Starting MongoDb 
+
+
 #!/bin/bash
 
 ID=$(id -u)
@@ -7,46 +59,40 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-TIMESTAMP= $(date +%F-%H-%M-%S)
+TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
-    echo -e "script started executing at $TIMESTAMP" &>>$LOGFILE
+echo -e "script started executing at $TIMESTAMP" &>>$LOGFILE
 
 VALIDATE () {
 
 if [ $1 -ne 0 ]
 then 
-    echo -e "$2......$R FAILED $N"
+    echo -e "$2......$R FAILED $N" &>>$LOGFILE
     exit 1
 else
-    echo -e "$2.....$G SUCCESS $N"
-
+    echo -e "$2.....$G SUCCESS $N" &>>$LOGFILE
 fi
 
 }
 
 if [ $ID -ne 0 ]
-
 then 
     echo -e "$R ERROR::Please run this script with root access $N"
     exit 1
 else
-    echo "you are root user "
+    echo "You are the root user." &>>$LOGFILE
 fi
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
-
 VALIDATE $? "Copied Mongodb repo"
 
 yum install mongodb-org -y &>>$LOGFILE
-
 VALIDATE $? "Installing Mongodb"
 
 systemctl start mongod &>>$LOGFILE
+VALIDATE $? "Starting MongoDB"
 
-VALIDATE $? "Enabling Mongodb"
+systemctl enable mongod &>>$LOGFILE
+VALIDATE $? "Enabling MongoDB"
 
-systemctl start mongodb &>>$LOGFILE
-
-VALIDATE $? Starting MongoDb 
-
-
+#echo -e "$G MongoDB installation completed successfully $N" &>>$LOGFILE
