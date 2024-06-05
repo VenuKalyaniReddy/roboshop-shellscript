3acc248506e4cda6b2981bb64809888f0f4bad2d
unknown <kalyaninalamaru.com>
2024-06-05 13:33:54 +0530

    roboshop shell scripts

--- mongodb.sh
+++ mongodb.sh
@@ -1,59 +1,6 @@
-# #!/bin/bash
-
-# ID=$(id -u)
-
-# R="\e[31m"
-# G="\e[32m"
-# Y="\e[33m"
-# N="\e[0m"
-
-# TIMESTAMP= $(date +%F-%H-%M-%S)
-# LOGFILE="/tmp/$0-$TIMESTAMP.log"
-#     echo -e "script started executing at $TIMESTAMP" &>>$LOGFILE
-
-# VALIDATE () {
-
-# if [ $1 -ne 0 ]
-# then 
-#     echo -e "$2......$R FAILED $N"
-#     exit 1
-# else
-#     echo -e "$2.....$G SUCCESS $N"
-
-# fi
-
-# }
-
-# if [ $ID -ne 0 ]
-
-# then 
-#     echo -e "$R ERROR::Please run this script with root access $N"
-#     exit 1
-# else
-#     echo "you are root user "
-# fi
-
-# cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
-
-# VALIDATE $? "Copied Mongodb repo"
-
-# yum install mongodb-org -y &>>$LOGFILE
-
-# VALIDATE $? "Installing Mongodb"
-
-# systemctl start mongod &>>$LOGFILE
-
-# VALIDATE $? "Enabling Mongodb"
-
-# systemctl start mongodb &>>$LOGFILE
-
-# VALIDATE $? Starting MongoDb 
-
-
 #!/bin/bash
 
 ID=$(id -u)
-
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -61,38 +8,39 @@ N="\e[0m"
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
-echo -e "script started executing at $TIMESTAMP" &>>$LOGFILE
 
-VALIDATE () {
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
 
-if [ $1 -ne 0 ]
-then 
-    echo -e "$2......$R FAILED $N" &>>$LOGFILE
-    exit 1
-else
-    echo -e "$2.....$G SUCCESS $N" &>>$LOGFILE
-fi
-
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
 }
 
 if [ $ID -ne 0 ]
-then 
-    echo -e "$R ERROR::Please run this script with root access $N"
-    exit 1
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
 else
-    echo "You are the root user." &>> $LOGFILE
-fi
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
-VALIDATE $? "Copied Mongodb repo"
 
-yum install mongodb-org -y &>> $LOGFILE
-VALIDATE $? "Installing Mongodb"
+VALIDATE $? "Copied MongoDB Repo"
 
-systemctl start mongod &>> $LOGFILE
-VALIDATE $? "Starting MongoDB"
+dnf install mongodb-org -y &>> $LOGFILE
+
+VALIDATE $? "Installing MongoDB"
 
 systemctl enable mongod &>> $LOGFILE
+
 VALIDATE $? "Enabling MongoDB"
 
-#echo -e "$G MongoDB installation completed successfully $N" &>>$LOGFILE
+systemctl start mongod &>> $LOGFILE
+
+VALIDATE $? "Starting MongoDB"
