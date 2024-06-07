1c6612722cf989bc562f1d76008c1a5766b55b93
unknown <kalyaninalamaru.com>
2024-06-07 15:37:26 +0530

    roboshop shell scripts

--- cart.sh
+++ cart.sh
@@ -16,7 +16,7 @@ then
     echo -e "$2 ......$R FAILED $N"
     exit 1
 else
-    echo -e "$2 ......$R SUCCESS $N"
+    echo -e "$2 ......$G SUCCESS $N"
 fi
 }
 
@@ -28,16 +28,16 @@ else
     echo "you are root user"
 fi
 
-dnf module disable nodejs -y
+dnf module disable nodejs -y &>>$LOGFILE
 VALIDATE $? "Disable current nodejs"
 
-dnf module enable nodejs:18 -y
+dnf module enable nodejs:18 -y &>>$LOGFILE
 VALIDATE $? "Enable current nodejs"
 
-dnf install nodejs -y
+dnf install nodejs -y &>>$LOGFILE
 VALIDATE $? "Install nodejs"
 
-id roboshop
+id roboshop &>>$LOGFILE
 if [ $? -ne 0 ]
 then 
     echo -e "create roboshop user"
@@ -46,31 +46,31 @@ else
     echo -e "roboshop user already created"
 fi
 
-mkdir -p /app
+mkdir -p /app &>>$LOGFILE
 VALIDATE $? "craeting app directory"
 
 cd /app 
 VALIDATE $? "Enter into app directory"
 
-curl -L -o /tmp/cart.zip https://roboshop-builds.s3.amazonaws.com/cart.zip
+curl -L -o /tmp/cart.zip https://roboshop-builds.s3.amazonaws.com/cart.zip &>>$LOGFILE
 VALIDATE $? "Download cart application into app directory"
 
-unzip -o /tmp/cart.zip
+unzip -o /tmp/cart.zip &>>$LOGFILE
 VALIDATE $? "Unzip the directory"
 
-npm install 
+npm install  &>>$LOGFILE
 VALIDATE $? "Install the dependencies"
 
-cp /home/centos/roboshop-shellscript/cart.service /etc/systemd/system/cart.service
+cp /home/centos/roboshop-shellscript/cart.service /etc/systemd/system/cart.service &>>$LOGFILE
 VALIDATE $? "copying the cart.service file"
 
-systemctl daemon-reload
+systemctl daemon-reload &>>$LOGFILE
 VALIDATE $? "realod-deamond"
 
-systemctl enable cart 
+systemctl enable cart  &>>$LOGFILE
 VALIDATE $? "enable cart"
 
-systemctl start cart
+systemctl start cart &>>$LOGFILE
 VALIDATE $? "start cart"
 
 
