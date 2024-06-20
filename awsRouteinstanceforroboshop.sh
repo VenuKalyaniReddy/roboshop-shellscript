661f686f2ca78fd9beacbc0ca19237d90e9f9d71
unknown <kalyaninalamaru.com>
2024-06-20 14:37:36 +0530

    roboshop-shellscript

--- awsRouteinstanceforroboshop.sh
+++ awsRouteinstanceforroboshop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 AMI="ami-0b4f379183e5706b9" #this keeps on changing
-SG_ID="sg-0673d44a67505798d" #replace with your SG ID
+SG_ID="sg-014eccb356d7ab4d2" #replace with your SG IDsg-014eccb356d7ab4d2
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 ZONE_ID=Z0321255HE9OPZ7QKB2P # replace your zone ID
 DOMAIN_NAME="aidevops.website"
