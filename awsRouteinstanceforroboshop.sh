bbf802b11f97463ec99a430828f40b04521cbeea
unknown <kalyaninalamaru.com>
2024-06-20 14:59:18 +0530

    roboshop-shellscript

--- awsRouteinstanceforroboshop.sh
+++ awsRouteinstanceforroboshop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 AMI="ami-0b4f379183e5706b9" #this keeps on changing
-SG_ID="sg-014eccb356d7ab4d2" #replace with your SG IDsg-014eccb356d7ab4d2
+SG_ID="sg-014eccb356d7ab4d2" #replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 ZONE_ID=Z0321255HE9OPZ7QKB2P # replace your zone ID
 DOMAIN_NAME="aidevops.website"
@@ -15,7 +15,7 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-0673d44a67505798d --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+ IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-014eccb356d7ab4d2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
 
    #create R53 record, make sure you delete existing record
